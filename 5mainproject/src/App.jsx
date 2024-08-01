71c6590283d2dedf8134ce2d663781b36763e81c
Viraj <virajworkstation13.gmail.com>
2024-08-01 23:03:49 +0530

    commit

--- 5mainproject/src/App.jsx
+++ 5mainproject/src/App.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
 import Display from './components/Display'
+import { useContext } from 'react'
+import { PlayerContext } from './context/PlayerContext'
+
 
 
 const App = () => {
+
+  const {audioRef} = useContext(PlayerContext)
+
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
@@ -12,6 +18,7 @@ const App = () => {
     <Display/>
       </div>
      <Player/>
+     <audio ref={audioRef} preload='auto'></audio>
     </div>
   )
 }
